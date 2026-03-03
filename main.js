1b9a786d102e9480a9ed7cb57edc3c20255fc177
Sakabaka <sakabakamail@gmail.com>
2026-03-03 10:01:10 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -8,8 +8,6 @@ camera.position.set(0,0,10);
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 
-const controls = new OrbitControls( camera, renderer.domElement );
-
 // add our renderer to the page     
 document.body.appendChild(renderer.domElement);
 
@@ -20,57 +18,17 @@ const light = new THREE.DirectionalLight(0xffffff, 1);
 
 scene.add(light);
 
-var mouse = new THREE.Vector2();
+let ImpObject; 
 
-window.addEventListener('pointerenter', (event) => {
+// import this on your HTML
+window.addEventListener('load', () => {
+    ImpObject = document.querySelector('[model]');
 
-    // Calculates pointer position in normalized device coordinates (-1 to +1)
-    mouse.x = (event.clientX / window.innerWidth) * 2 - 1;
-    mouse.y = -(event.clientY / window.innerHeight) * 2 + 1;
-    const raycaster = new THREE.Raycaster();
-    // In your animate loop or click handler:
-    raycaster.setFromCamera(mouse, camera);
-
-    // Intersect with specific objects
-    const intersects = raycaster.intersectObjects(scene.children);
-    if (intersects.length > 0) 
-    {
- 
-    }
+    const camera = document.querySelector('[camera]');
+    const marker = document.querySelector('a-marker');
+    let check;
 });
 
-
-window.addEventListener('mousedown', (event) =>{
-    console.log("touching");
-});
-
-window.addEventListener('mouseup', (event) =>{
-    console.log("stop touching");
-});
-
-
-window.addEventListener("resize", () => {
-    // Mise à jour du ratio de la caméra
-    camera.aspect = window.innerWidth / window.innerHeight;
-    // Mise à jour de la matrice de projection
-    camera.updateProjectionMatrix();
-    // Mise à jour de la taille du renderer
-    renderer.setSize(window.innerWidth, window.innerHeight);
-});
-
-
-const timer = new THREE.Timer();
-
-function animate(timestamp) {
-  requestAnimationFrame(animate);
-  // timestamp is optional
-  timer.update(timestamp);
-  const delta = timer.getDelta();
-  const elapsedTime = timer.getElapsed();
-
-
-
-  renderer.render(scene, camera);
-}
-
-animate();
+window.addEventListener('click',(Event) => {
+    console.log(ImpObject.object3D3.scale);
+})
